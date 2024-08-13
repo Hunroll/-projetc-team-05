830e8a136223c69b1d639d54bb22411065849a6d
Bohdan Horobets <91347979+Guru01100101@users.noreply.github.com>
2024-08-13 21:32:52 +0300

    Add normalize_phone.py with validator of phone number and use normalize_phone() function in Phone class constructor.
    Add docstrings to exciting classes and methods.

--- src/models.py
+++ src/models.py
@@ -1,8 +1,12 @@
-import re
 from datetime import datetime
+from typing import Any, List
+
+from src.normalize_phone import normalize_phone
+
 
 class Field:
-    def __init__(self, value):
+    """Base class for representing fields with a value."""
+    def __init__(self, value: Any):
         self.value = value
 
     def __str__(self):
@@ -12,65 +16,92 @@ class Field:
         return other.__class__ == self.__class__ and other.value == self.value
     
 class Name(Field):
+    """Class for representing a name field."""
     pass
 
 class Phone(Field):
-    def __init__(self, value):
-        if (not re.match(r'^\d{10,10}$', value)):
-            raise ValueError("Incorrect phone number")
-        super().__init__(value)
+    """Class for representing a phone field.
+    use normalize_phone function to normalize and validate phone number
+    __init__:
+        phone: str
+            The phone number of the contact in format '0XXXXXXXXX' or '+380XXXXXXXXX'
+    __errors_handling__:
+        ValueError: if the phone number does not match any of the known formats.
+    """
+    def __init__(self, value: str):
+        super().__init__(normalize_phone(value))
 
 class Birthday(Field):
-    def __init__(self, value):
+    """Class for representing a birthday field.
+    __init__:
+        value: str
+            The birthday of the contact in format DD.MM.YYYY
+    __errors_handling__:
+        ValueError: if the date does not match the format or is a future date
+    """
+    def __init__(self, value: str):
         try:
             value = datetime.strptime(value, "%d.%m.%Y")
         except ValueError:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
         if value > datetime.now():
             raise ValueError("Birthday can\'t be a future date")
-        self.value = value
+        super().__init__(value)
     
     def __str__(self):
         return datetime.strftime(self.value, "%d.%m.%Y")
 
         
 class Record:
-    def __init__(self, name):
+    """Class for representing a contact record.
+    __init__:
+        name: str
+            The name of the contact
+        """
+    def __init__(self, name: str, phones: List[str]=None, birthday: str=None):
         self.name = Name(name)
-        self.__phones = []
-        self.birthday = None
+        if phones is None:
+            phones = []
+        else:
+            phones = [Phone(p) for p in phones]
+        self.__phones = phones
+        if birthday:
+            self.add_birthday(birthday)
+        else:
+            self.birthday = None
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, birthday: {self.birthday if self.birthday else 'Not set'}, phones: {'; '.join(self.phones)}"
+        return f"Contact name: {self.name.value}, \nbirthday: {self.birthday if self.birthday else 'Not set'}, \nphones: {'; '.join(self.phones)}"
     
-    def add_phone(self, phone):
+    def add_phone(self, phone: str):
         phone = Phone(phone)
         if phone not in self.__phones:
             self.__phones.append(phone)
         else:
-            raise ValueError("This number already exists")
+            raise ValueError(f"[ERROR] Number {phone} already exists in the record {self.name.value}")
 
-    def remove_phone(self, phone):
+    def remove_phone(self, phone: str):
         phone = Phone(phone)
         self.__phones.remove(phone)
 
-    def edit_phone(self, phone, new_phone):
+    def edit_phone(self, phone: str, new_phone: str):
         phone = Phone(phone)
         new_phone = Phone(new_phone)
         if phone not in self.__phones:
             raise ValueError(f"{phone.value} does not exist")
-        if new_phone in self.__phones:
+        elif new_phone in self.__phones:
             raise ValueError(f"{new_phone.value} already exists")
-        self.__phones = list(map(lambda p: p if p != phone else new_phone, self.__phones))
+        else:
+            self.__phones = list(map(lambda p: p if p != phone else new_phone, self.__phones))
 
     @property
     def phones(self):
         return (p.value for p in self.__phones)
         
 
-    def find_phone(self, phone) -> Phone:
+    def find_phone(self, phone: Phone) -> Phone|None:
         return phone if phone in self.phones else None
 
-    def add_birthday(self, birthday):
+    def add_birthday(self, birthday: str):
         birthday = Birthday(birthday)
         self.birthday = birthday
