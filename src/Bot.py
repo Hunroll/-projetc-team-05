1162662704bda5254222ed4c3ba98aa2b94aecd9
b.o.gorobets <b.o.gorobets@gmail.com>
2024-08-14 16:48:50 +0300

    Redo search command:   Remove code from Bot -> AddressBook.search(pattern)
    Refactor code to PEP8:
      - remove brackets
      - add @staticmethod
      - rename dict -> birth_dict inside birthdays() (bad use stnd words to name variables)

--- src/Bot.py
+++ src/Bot.py
@@ -6,7 +6,8 @@ from src.models import *
 
 CMD_EXIT="exit"
 CMD_NA="n/a"
-class Bot():
+class Bot:
+    @staticmethod
     def input_error(func):
         def inner(*args, **kwargs) -> str:
             try:
@@ -20,25 +21,29 @@ class Bot():
         return inner
 
     #returns command key and args if any
+    @staticmethod
     def parse_input(inp: str) -> tuple[str, tuple]:
         cmd, *args = inp.split()
         cmd = cmd.lower()
         return cmd, *args
 
+    @staticmethod
     @input_error
     def say_hello(contacts: AddressBook, *args) -> str:
-        if (len(*args)):
+        if len(*args):
             raise IndexError("\"hello\" doesn\'t need arguments")
-        return "How can I help you?"    
+        return "How can I help you?"
 
+    @staticmethod
     @input_error
     def say_goodbye(contacts: AddressBook, *args) -> str:
         contacts.save_data()
         return "DB is saved. Good bye!"
 
+    @staticmethod
     @input_error
     def add_contact(contacts: AddressBook, *args) -> str:
-        if (len(*args) != 2):
+        if len(*args) != 2:
             raise ValueError("Incorrect number of arguments." + Fore.YELLOW + " Please try \"add _name_ _phone_\"")
         name, phone, *_ = args[0]
         mess = "Contact updated."
@@ -49,10 +54,11 @@ class Bot():
             mess = "Contact added."
         contact.add_phone(phone)
         return mess
-        
+
+    @staticmethod
     @input_error
     def change_contact(contacts: AddressBook, *args) -> str:
-        if (len(*args) != 3):
+        if len(*args) != 3:
             raise ValueError("Incorrect number of arguments." + Fore.YELLOW + " Please try \"change _name_ _old_phone_ _new_phone_\"")
         name, old_phone, new_phone, *_ = args[0]
         contact = contacts.find(name)
@@ -62,9 +68,10 @@ class Bot():
         contact.edit_phone(old_phone, new_phone)
         return "Contact updated."
 
+    @staticmethod
     @input_error
     def get_phone(contacts: AddressBook, *args) -> str:
-        if (len(*args) != 1):
+        if len(*args) != 1:
             raise IndexError("Incorrect number of arguments" + Fore.YELLOW + " Please try \"phone _name_ \"")
         name, *_ = args[0]
         contact = contacts.find(name)
@@ -72,31 +79,21 @@ class Bot():
             raise KeyError("Contact doesn\'t exist")
         return str.join("; ", contact.phones)
 
-
+    @staticmethod
     @input_error
     def search_contact(contacts: AddressBook, *args) -> str:
-        if (len(*args) != 1):
+        if len(*args) != 1:
             raise IndexError("Incorrect number of arguments" + Fore.YELLOW + " Please try \"search _name_ \"")
         pattern, *_ = args[0]
 
-        found = False
+        return str.join("\n", [str(contact) for contact in contacts.search(pattern)])
 
-        result = ""
-        for name, record in contacts.items():
-            if pattern.lower() in name.lower() or any(pattern.lower() in phone for phone in record.phones):
-                found = True
-                result += f"{record}\n"
-
-        if not found:
-            return "No matches found."
-
-        return result
-
+    @staticmethod
     @input_error
     def get_all(contacts: AddressBook, *args) -> str:
-        if (len(*args)):
+        if len(*args):
             raise IndexError("\"all\" doesn\'t need arguments")
-        if (len(contacts) == 0):
+        if len(contacts) == 0:
             return "It\'s lonely here:( Please use \"add\" command"
         result_str = "{:<20} {:<12} {:<20} {:<20} {:<20}\n".format("Name", "Birthday", "Phone(s)", "Email", "Address")
         for k, user in contacts.items():
@@ -108,9 +105,10 @@ class Bot():
                 str(user.address) if user.address else 'Not set' )
         return result_str
 
+    @staticmethod
     @input_error
     def add_birthday(contacts: AddressBook, *args) -> str:
-        if (len(*args) != 2):
+        if len(*args) != 2:
             raise ValueError("Incorrect number of arguments." + Fore.YELLOW + " Please try \"add-birthday _name_ _DD.MM.YYYY_\"")
         name, birthday, *_ = args[0]
         
@@ -120,10 +118,11 @@ class Bot():
         
         contact.add_birthday(birthday)
         return "Contact updated."
-        
+
+    @staticmethod
     @input_error
     def show_birthday(contacts: AddressBook, *args) -> str:
-        if (len(*args) != 1):
+        if len(*args) != 1:
             raise IndexError("Incorrect number of arguments" + Fore.YELLOW + " Please try \"show-birthday _name_ \"")
         name, *_ = args[0]
         contact = contacts.find(name)
@@ -131,19 +130,21 @@ class Bot():
             raise KeyError("Contact doesn\'t exist")
         return str(contact.birthday) if contact.birthday else f"{name} doesn\'t have birthday set"
 
+    @staticmethod
     @input_error
     def birthdays(contacts: AddressBook, *args) -> str:
-        if (len(*args)):
+        if len(*args):
             raise IndexError("\"birthdays\" doesn\'t need arguments")
-        dict = contacts.get_upcoming_birthdays()
+        birth_dict = contacts.get_upcoming_birthdays()
         result_str = ""
-        for bd in dict:
+        for bd in birth_dict:
             result_str += f"{str(bd['name']) : <20}{bd['congratulation_date'] : <20}\n"
         return result_str
-    
+
+    @staticmethod
     @input_error
     def add_email(contacts: AddressBook, *args) -> str:
-        if (len(*args) != 2):
+        if len(*args) != 2:
             raise ValueError("Incorrect number of arguments." + Fore.YELLOW + " Please try \"add-email _name_ _email@example.com_\"")
         name, email, *_ = args[0]
         
@@ -154,9 +155,10 @@ class Bot():
         contact.add_email(email)
         return "Contact updated."
 
+    @staticmethod
     @input_error
     def add_address(contacts: AddressBook, *args) -> str:
-        if (len(*args) < 2):
+        if len(*args) < 2:
             raise ValueError("Incorrect number of arguments." + Fore.YELLOW + " Please try \"add-address _name_ _address_\"")
 
         name = args[0][0]
@@ -169,6 +171,7 @@ class Bot():
         contact.add_address(address)
         return "Contact updated."
 
+    @staticmethod
     def register_handlers() -> dict:
         funcs = dict()
         funcs["hello"] = Bot.say_hello
